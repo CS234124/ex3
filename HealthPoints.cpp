dbb59142c650a70ef5896dc7195fc01eda8ff44a
ItamarParienty <parienty.itamar@gmail.com>
2022-05-25 17:18:35 +0300

    Finished working on HealthPoints class. Need to test

---
+++ HealthPoints.cpp
@@ -0,0 +1,79 @@
+//
+// Created by User on 21/05/2022.
+//
+
+#include "HealthPoints.h"
+
+const int NO_HEALTH_POINTS = 0;
+
+HealthPoints::HealthPoints(const int maxHealthPoints) :
+        m_maxHealthPoints(maxHealthPoints), m_healthPoints(maxHealthPoints) {
+    if (maxHealthPoints <= NO_HEALTH_POINTS) {
+        throw InvalidArgument();
+    }
+}
+
+HealthPoints &HealthPoints::operator=(const HealthPoints &other) {
+    if (this != &other) {
+        m_maxHealthPoints = other.m_maxHealthPoints;
+        m_healthPoints = other.m_healthPoints;
+    }
+    return *this;
+}
+
+HealthPoints &HealthPoints::operator-=(const int &subtrahend) {
+    *this += -subtrahend;
+    return *this;
+}
+
+HealthPoints HealthPoints::operator-(const int &subtrahend) const {
+    HealthPoints resultHealthPoints = *this;
+    resultHealthPoints -= subtrahend;
+    return resultHealthPoints;
+}
+
+HealthPoints &HealthPoints::operator+=(const int &addend) {
+    m_healthPoints += addend;
+    if (m_healthPoints < 0) {
+        m_healthPoints = 0;
+    }
+    if (m_healthPoints > m_maxHealthPoints) {
+        m_healthPoints = m_maxHealthPoints;
+    }
+    return *this;
+}
+
+HealthPoints HealthPoints::operator+(const int &addend) const {
+    HealthPoints resultHealthPoints = *this;
+    resultHealthPoints += addend;
+    return resultHealthPoints;
+}
+
+bool HealthPoints::operator==(const HealthPoints &other) const {
+    return m_healthPoints == other.m_healthPoints;
+}
+
+bool HealthPoints::operator!=(const HealthPoints &other) const {
+    return m_healthPoints != other.m_healthPoints;
+}
+
+bool HealthPoints::operator>(const HealthPoints &other) const {
+    return m_healthPoints > other.m_healthPoints;
+}
+
+bool HealthPoints::operator>=(const HealthPoints &other) const {
+    return m_healthPoints >= other.m_healthPoints;
+}
+
+bool HealthPoints::operator<(const HealthPoints &other) const {
+    return m_healthPoints < other.m_healthPoints;
+}
+
+bool HealthPoints::operator<=(const HealthPoints &other) const {
+    return m_healthPoints <= other.m_healthPoints;
+}
+
+std::ostream &operator<<(std::ostream &os, const HealthPoints &healthPoints) {
+    os<<healthPoints.m_healthPoints<<"("<<healthPoints.m_maxHealthPoints<<")";
+    return os;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include "HealthPoints.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    HealthPoints hp1 = HealthPoints(150);
+    HealthPoints hp2 = 100;
+    HealthPoints hp3 = hp1 - 10;
+    hp1 -= 30;
+    hp1 += 20;
+    hp2 = hp3 + 5;
+    hp1 = 10 + hp3;
+    if (hp1 == hp2) {
+        std::cout << "1" << std::endl;
+    }
+    if (hp1 == 100) {
+        std::cout << "1" << std::endl;
+    }
+    if (hp1 > 100) {
+        std::cout << "1" << std::endl;
+    }
+    if (hp1 >= 100) {
+        std::cout << "1" << std::endl;
+    }
+    if (100 == hp1) {
+        std::cout << "1" << std::endl;
+    }
+    if (100 <= hp1) {
+        std::cout << "1" << std::endl;
+    }
+    if (100 < hp1) {
+        std::cout << "1" << std::endl;
+    }
+    std::cout << hp2 << std::endl;
+    std::cout << hp2 << "asda" << std::endl;
+    std::cout << "asda" << hp2 << std::endl;
+    std::cout << "asda" << hp2 << "asda" << std::endl;
+    std::cout << hp2 << "asda" << hp1 << std::endl;
     return 0;
 }
