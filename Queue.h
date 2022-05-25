7938ae13040266306a6d9a0226992932b4b3bd51
matan <matan.levin@campus.technion.ac.il>
2022-05-25 12:09:27 +0300

    added exceptions before debugging

--- Queue.h
+++ Queue.h
@@ -4,8 +4,6 @@
 template <typename T> class Node
 {
     public:
-    // bunch of types
-
     Node( T member) {
         this->m_value = member;
         this->m_next = NULL;
@@ -19,55 +17,13 @@ template <typename T> class Node
 template <typename T>
 class Queue {
 
-    public:
+public:
 
     Queue(){
         m_front=NULL;
         m_rear=NULL;
         m_size=0;
             }
-
-    class Iterator{
-    public:
-        const T& operator*() const{
-            return m_current->m_value;
-        }
-
-        Iterator& operator++(){
-            m_current=m_current->m_next;
-            return *this;
-        }
-        Iterator& operator++(int){
-            return ++*this;
-        }
-
-        //TODO need a checkup
-        bool operator!=(const Iterator& it) const{
-            if(queue==it.queue){
-                return m_current == it.m_current;
-            }
-            else{
-                return false;
-            }
-        }
-
-    private:
-        const Queue<T>* queue;
-        Node<T>* m_current;
-        Iterator(const Queue<T>* queue , Node<T>* m_current){
-            this->queue = queue;
-            this->m_current = m_current;
-        }
-        friend class Queue<T>;
-
-    };
-    Iterator begin() const{
-        return Iterator(this, m_front);
-    }
-    Iterator end() const{
-        return Iterator(this, m_rear);
-    }
-
 
     //declaring and writing the functions
     bool isEmpty() {
@@ -88,16 +44,15 @@ class Queue {
     }
     Node<T>* Front() {
             if (isEmpty()){
-                return NULL;
+                throw EmptyQueue();
             }
-
             return m_front;
 
     }
     Node<T>* PopFront() {
             Node<T>* temp=NULL;
             if(isEmpty()){
-                return NULL;
+                throw EmptyQueue();
             }
 
             else{
@@ -121,35 +76,97 @@ class Queue {
             return m_size;
     }
 
+    //exception
+    class EmptyQueue{};
+
+
+    //iterator definition
+    class Iterator{
+    public:
+        //exception
+        class InvalidOperation{};
+
+        //constructor and defining
+        const T& operator*() const{
+
+            if(*this==NULL){
+                throw InvalidOperation{};
+            }
+
+            return m_current->m_value;
+        }
+
+        Iterator& operator++(){
+
+            if(*this==NULL){
+                throw InvalidOperation{};
+            }
+
+            m_current=m_current->m_next;
+            return *this;
+        }
+        Iterator& operator++(int){
+            return ++*this;
+        }
+
+        //TODO need a checkup
+        bool operator!=(const Iterator& it) const{
+            if(queue==it.queue){
+                return m_current == it.m_current;
+            }
+            else{
+                return false;
+            }
+        }
+
+
+
     private:
+        const Queue<T>* queue;
+        Node<T>* m_current;
+        Iterator(const Queue<T>* queue , Node<T>* m_current){
+            this->queue = queue;
+            this->m_current = m_current;
+        }
+        friend class Queue<T>;
+
+    };
+
+    Iterator begin() const{
+        return Iterator(this, m_front);
+    }
+    Iterator end() const{
+        return Iterator(this, m_rear);
+    }
+
+
+private:
 
     Node<T>* m_rear, m_front;
     int m_size;
 
 };
 
-//TODO do we need to keep an old copy? they didnt specify + take into account having the iteration on an empty queue
-//TODO consult if to allocate a new queue or edit the one and how
 template <typename T>
 Queue<T> Filter (Queue<T> CurrentQueue , bool(&conditionFunction)(T)) {
 
-
-
-    //TODO not sure if i need const &T or not.
-    for( T& element : CurrentQueue){
-        if(conditionFunction(CurrentQueue.m_value)==false){
-            CurrentQueue.pushBack(element);
+    Queue<T> queue;
+    for( const T& element : CurrentQueue){
+        if(conditionFunction(element.m_value)){
+            queue.pushBack(element);
         }
     }
-    return CurrentQueue;
+    return queue;
 };
 
-template <typename T>
-Queue<T> transform (Queue<T> CurrentQueue , void(&TransformFunction)(T)) {
-    for( T& element : CurrentQueue){
-        TransformFunction(element->m_value);
+template <typename T, typename U>
+Queue<T> transform (Queue<T> CurrentQueue , U (&TransformFunction)(T)) {
+
+    Queue<T> queue;
+    for( const T& element : CurrentQueue){
+        queue.pushBack(TransformFunction(&element->m_value));
         }
-    return CurrentQueue;
+    return queue;
 };
 
 
